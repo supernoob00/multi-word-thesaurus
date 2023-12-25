e4adc4076d2b369f5b30423b69f0a6344be6e92a
ImJustAnotherNoob <sjsomerdin@gmail.com>
2023-12-25 12:03:07 -0500

    Synonym display box now disappears after clicking

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,3 +1,5 @@
+import {postWordsDto, getSynonymList} from "./service.js";
+
 const wordsSubmitForm = document.querySelector(".words-submit-form");
 
 wordsSubmitForm.addEventListener("submit", async e => {
@@ -102,6 +104,11 @@ function createNetwork(wordGraphData) {
     });
 
     network.on('click', function(properties) {
+        const synonymDisplay = document.body.querySelector(".synonyms-container");
+        if (synonymDisplay !== null) {
+            document.body.removeChild(synonymDisplay);
+        }
+
         const nodeId = network.getNodeAt({x:properties.event.srcEvent.offsetX, y:properties.event.srcEvent.offsetY});
         if (nodeId === undefined) {
             return;
@@ -137,39 +144,7 @@ function displayEmptySynonyms() {
 
 }
 
-const BASE_URL = 'http://localhost:8080';
 
-async function postWordsDto(wordsDto) {
-    // Default options are marked with *
-    const response = await fetch(BASE_URL + "/wordgraph", {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(wordsDto), // body data type must match "Content-Type" header
-    });
-    return response;
-}
-
-async function getSynonymList(word) {
-    // Default options are marked with *
-    const response = await fetch(BASE_URL + `/words/${word}`, {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    });
-    return response;
-}
 
 
         
---
+++ src/main/webapp/service.js
@@ -0,0 +1,33 @@
+const BASE_URL = 'http://localhost:8080';
+
+export async function postWordsDto(wordsDto) {
+    // Default options are marked with *
+    const response = await fetch(BASE_URL + "/wordgraph", {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(wordsDto), // body data type must match "Content-Type" header
+    });
+    return response;
+}
+
+export async function getSynonymList(word) {
+    // Default options are marked with *
+    const response = await fetch(BASE_URL + `/words/${word}`, {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
+    return response;
+}
