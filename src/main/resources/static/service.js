ceb38866ecf0a4ef393c9492d2fec3e1944cd829
ImJustAnotherNoob <sjsomerdin@gmail.com>
2023-12-26 19:03:03 -0500

    Changed url in client service

--- src/main/resources/static/service.js
+++ src/main/resources/static/service.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://wordweb.up.railway.app';
 
 export async function postWordsDto(wordsDto) {
     // Default options are marked with *
