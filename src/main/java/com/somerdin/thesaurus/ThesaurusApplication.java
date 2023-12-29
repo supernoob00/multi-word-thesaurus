b0e62a934288172165e029e0553fb840e0ef8346
ImJustAnotherNoob <sjsomerdin@gmail.com>
2023-12-28 20:25:06 -0500

    Removed vis.js file

--- src/main/java/com/somerdin/thesaurus/ThesaurusApplication.java
+++ src/main/java/com/somerdin/thesaurus/ThesaurusApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.somerdin.thesaurus"})
 public class ThesaurusApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThesaurusApplication.class, args);
--- src/main/resources/static/service.js
+++ src/main/resources/static/service.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://wordweb.up.railway.app';
+const BASE_URL = 'http://localhost:8080';
 
 export async function postWordsDto(wordsDto) {
     // Default options are marked with *
